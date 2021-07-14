d84554b0dfa8579513416f944b57659daca0c1ff
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-07-14 18:48:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,122 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // 1 task:
+        // Write a program that counts in descending order 100 -0;
+        // Print out only the values that are divisible by 6
+//        for (int i = 100; i>=0; i--) {
+//            if(i % 6 == 0) {
+//                System.out.println(i);
+ //           }
+ //       }
+        // 2 task
+        // Write a program that takes input from the user for 10 numbers and
+        // find the average of those numbers.
+
+        // 3rd task
+        // Write a program that takes in 5 strings from the user
+        // and concatenates those strings into one sentence.
+        // Enter a string: Harry
+        // Enter a string: is
+        // ....
+        // Harry is ...
+
+
+        //4th task
+        // Write a program that allows takes in 10 numbers as an array and finds the
+        // the largest number.
+
+        // 5th task:
+        // Using an array, output the number of odd numbers
+        // out of the numbers a user has entered. i.e. allow the
+        // user to enter a number of numbers and return or output
+        // the number of numbers.
+
+        // Enter how many numbers you want: 6
+        // enter a number
+        //enter a number
+        //...
+        // the number of odd number is
+
+        //6th task
+        // Create a class called Vehicle. Let this car have features like,
+        // length of the vehicle, the number of seats,
+        // no of tires, name/brand of the vehicle, current speed.
+        // methods include: speed up(increases speed by 5), slow down(reduce speed by 5), start engine, stop engine.
+
+        // 2nd task
+           Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Enter number: ");
+//        int num1 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num2 = scanner.nextInt();
+
+ //       System.out.print("Enter number: ");
+//        int num3 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num4 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num5 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num6 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num7 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num8 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num9 = scanner.nextInt();
+
+//        System.out.print("Enter number: ");
+//        int num10 = scanner.nextInt();
+
+//        System.out.println("Average is: " + (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10) / 10);
+
+            //3rd task
+
+//       System.out.println("Enter a string: ");
+//        String word1 = scanner.next();
+
+//        System.out.println("Enter a string: ");
+//        String word2 = scanner.next();
+
+//        System.out.println("Enter a string: ");
+//       String word3 = scanner.next();
+
+//        System.out.println("Enter a string: ");
+//        String word4 = scanner.next();
+
+//        System.out.println("Enter a string: ");
+//        String word5 = scanner.next();
+
+//        System.out.println(word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5);
+
+        //4th task
+//        int[] numbers = new int[] { 9, 70, 15, 4, 26, 49, 11, 58, 99, 1};
+//        int max = numbers[0];
+//        for(int i = 1; i < numbers.length;i++) {
+ //           if (numbers[i] > max) {
+ //               max = numbers[i];
+//            }
+//        }
+//        System.out.println("Array is: ");
+//        for(int i = 0; i < numbers.length;i++){
+//            System.out.println(numbers[i]);
+ //       } System.out.println("The Largest number is:" + max);
+
+        //5th task
+
+
+    }
+}
